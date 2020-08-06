9b0deeb5d93f54782b8d454e77508d3db9fdc556
Jakub Dolejs <jakubdolejs@gmail.com>
2020-08-06 18:09:33 +0100

    Added version in About activity menu

--- app/src/main/java/com/appliedrec/credentials/app/AboutActivity.java
+++ app/src/main/java/com/appliedrec/credentials/app/AboutActivity.java
@@ -1,8 +1,12 @@
 package com.appliedrec.credentials.app;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.appliedrec.verid.core.VerID;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        try {
+            String appVersion = "App version: "+getPackageManager().getPackageInfo(getPackageName(), 0).versionName;
+            menu.add(appVersion);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        String veridVersion = "Ver-ID SDK version: "+VerID.getVersion();
+        menu.add(veridVersion);
+        return true;
     }
 }
