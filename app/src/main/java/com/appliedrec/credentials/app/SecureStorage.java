16a3373e441d5bfd30f6dd12c2a8aee2fcd9ebf7
Jakub Dolejs <jakubdolejs@gmail.com>
2021-10-20 17:47:36 +0100

    Replace deprecated API use

--- app/src/main/java/com/appliedrec/credentials/app/SecureStorage.java
+++ app/src/main/java/com/appliedrec/credentials/app/SecureStorage.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 import androidx.security.crypto.EncryptedSharedPreferences;
-import androidx.security.crypto.MasterKeys;
+import androidx.security.crypto.MasterKey;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -19,7 +19,7 @@ class SecureStorage {
     }
 
     SecureStorage(Context context) throws GeneralSecurityException, IOException {
-        sharedPreferences = EncryptedSharedPreferences.create("credentialsAppSecureStore", MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC), context, EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV, EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
+        sharedPreferences = EncryptedSharedPreferences.create(context, "credentialsAppSecureStore", new MasterKey.Builder(context).setKeyScheme(MasterKey.KeyScheme.AES256_GCM).build(), EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV, EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM);
     }
 
     void setValueForKey(String value) {
